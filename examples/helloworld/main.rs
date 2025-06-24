9b0de6b13396c63202c6116f3e606fa19582c343
Monty C <211852937+montyc1999@users.noreply.github.com>
2025-06-24 14:37:12 -0400

    add 100 async commands to examples/helloworld to demo rustc_version speedup

--- examples/helloworld/main.rs
+++ examples/helloworld/main.rs
@@ -9,6 +9,409 @@ fn greet(name: &str) -> String {
   format!("Hello {name}, You have been greeted from Rust!")
 }
 
+pub struct MyState {}
+
+#[tauri::command]
+async fn _async_cmd_00(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_01(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_02(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_03(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_04(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_05(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_06(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_07(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_08(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_09(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_10(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_11(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_12(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_13(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_14(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_15(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_16(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_17(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_18(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_19(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_20(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_21(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_22(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_23(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_24(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_25(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_26(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_27(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_28(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_29(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_30(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_31(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_32(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_33(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_34(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_35(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_36(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_37(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_38(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_39(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_40(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_41(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_42(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_43(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_44(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_45(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_46(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_47(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_48(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_49(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_50(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_51(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_52(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_53(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_54(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_55(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_56(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_57(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_58(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_59(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_60(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_61(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_62(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_63(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_64(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_65(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_66(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_67(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_68(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_69(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_70(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_71(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_72(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_73(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_74(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_75(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_76(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_77(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_78(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_79(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_80(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_81(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_82(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_83(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_84(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_85(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_86(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_87(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_88(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_89(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_90(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_91(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_92(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_93(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_94(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_95(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_96(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_97(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_98(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+#[tauri::command]
+async fn _async_cmd_99(_: tauri::State<'_, MyState>) -> Result<(), ()> {
+  Ok(())
+}
+
 fn main() {
   tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![greet])
